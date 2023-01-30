a6157991e4ef709030ff1cc1c20b732afcaab25b
skanehira <sho19921005@gmail.com>
2023-01-30 19:10:31 +0900

    refactor: use debounce module

--- watcher.ts
+++ watcher.ts
@@ -1,4 +1,5 @@
 import { renderMarkdown } from "./render.ts";
+import { debounce } from "debounce";
 
 export class Watcher {
   #ws: WebSocket;
@@ -16,35 +17,23 @@ export class Watcher {
 
   async watch() {
     console.log(`watching ${this.#file}...`);
-    const watcher = Deno.watchFs(this.#file);
-
-    // エディタによるファイルへの書き込みでは
-    // Deno.watchFs は複数イベントが発火するので
+    // エディタによるファイルへの書き込みでは、
+    // Deno.watchFs は複数イベントが発火するので、
     // レンダリング後は一定期間は(0.1秒)レンダリングを行わない
-    const renderer = () => {
-      let before = Date.now();
-      return async (render: () => Promise<void>) => {
-        const elasped = Date.now() - before;
-        if (elasped < 100) {
-          return;
-        }
-        console.log("rendering...");
-        await render();
-        before = Date.now();
-      };
-    };
+    const render = debounce((ws: WebSocket, content: string) => {
+      console.log("redering...");
+      ws.send(renderMarkdown(content));
+    }, 200);
 
-    const render = renderer();
-
+    const watcher = Deno.watchFs(this.#file);
     for await (const event of watcher) {
       if (this.shouldClose()) {
         break;
       }
-      await render(async () => {
-        const file = event.paths[0];
-        const content = await Deno.readTextFile(file);
-        this.#ws.send(renderMarkdown(content));
-      });
+
+      const file = event.paths[0];
+      const content = await Deno.readTextFile(file);
+      render(this.#ws, content);
     }
   }
 }
