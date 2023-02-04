4f986944381602f5f02ff05ff8bd618679749d8c
skanehira <sho19921005@gmail.com>
2023-02-04 11:04:16 +0900

    fix: cannot serve static files

--- main.ts
+++ main.ts
@@ -4,4 +4,17 @@ import { router } from "./router.ts";
 const port = 9000;
 console.log(`Listening localhost: ${port}`);
 
-await new Application().use(router.routes()).listen({ port });
+const app = new Application();
+app.use(async (ctx, next) => {
+  try {
+    await ctx.send({
+      root: `${Deno.cwd()}`,
+      index: "/",
+    });
+  } catch {
+    await next();
+  }
+});
+app.use(router.routes());
+
+await app.listen({ port });
