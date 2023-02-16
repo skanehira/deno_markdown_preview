862b38bd90f18d268852c7a94d18d39a39f6b401
skanehira <sho19921005@gmail.com>
2023-02-16 20:47:33 +0900

    chore: refactor watcher.ts

--- watcher.ts
+++ watcher.ts
@@ -10,28 +10,26 @@ export class Watcher {
     this.#file = file;
   }
 
-  shouldStop(): boolean {
+  #shouldStop(): boolean {
     const state = this.#ws.readyState;
     return WebSocket.CLOSED === state || WebSocket.CLOSING === state;
   }
 
   async watch() {
     console.log(`watching ${this.#file}...`);
-    // エディタでファイルを更新すると複数のイベントが発生するため、
-    // レンダリング後は一定期間は(0.2秒)レンダリングを行わない
-    const render = debounce((ws: WebSocket, content: string) => {
+    const render = debounce((ws: WebSocket, file: string) => {
+      const content = Deno.readTextFileSync(file);
       ws.send(renderMarkdown(content));
     }, 200);
 
     const watcher = Deno.watchFs(this.#file);
     for await (const event of watcher) {
-      if (this.shouldStop()) {
+      if (this.#shouldStop()) {
         break;
       }
 
       const file = event.paths[0];
-      const content = await Deno.readTextFile(file);
-      render(this.#ws, content);
+      render(this.#ws, file);
     }
   }
 }
